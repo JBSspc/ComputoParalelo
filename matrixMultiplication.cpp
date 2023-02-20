e58bbc093b054433eac4a9c1fc15d31d62ae4941
Sofía Palacios <77463414+JBSspc@users.noreply.github.com>
2023-02-19 22:03:28 -0600

    Update matrixMultiplication.cpp

--- matrixMultiplication.cpp
+++ matrixMultiplication.cpp
@@ -19,32 +19,7 @@ void initMat();
 int serialM[3][3];
 int parallelM[3][3];
 
-void MultiplyWithOutAMP() {
-    int aMatrix[3][2] = {{1, 4}, {2, 5}, {3, 6}};
-    int bMatrix[2][3] = {{7, 8, 9}, {10, 11, 12}};
-    int product[3][3] = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
-
-    for (int row = 0; row < 3; row++) {
-        for (int col = 0; col < 3; col++) {
-            // Multiply the row of A by the column of B to get the row, column of product.
-            for (int inner = 0; inner < 2; inner++) {
-                product[row][col] += aMatrix[row][inner] * bMatrix[inner][col];
-            }
-            cout << product[row][col] << "  ";
-        }
-        cout << "\n";
-    }
-}
-
 int main() {
-    // Inicializar vectores con números aleatorios
-    /*
-    auto start = high_resolution_clock::now();
-    initMat();
-    auto end = high_resolution_clock::now();
-    auto duration = duration_cast<microseconds>(end - start);
-    cout << "Initialization finished, elapsed time: "<<  duration.count()/1000 << " milliseconds" <<endl;
-    */
 
     // Multiplicación serial
     auto start = high_resolution_clock::now();
@@ -67,14 +42,6 @@ int main() {
     getchar();
 }
 
-// Función para inicializar las matrices
-/*
-void initMat(){
-    A[2][2]={ {1,2}, {3,4}};
-    B[2][2]={ {5,6}, {7,8}};
-
-}
-*/
 void serialMultiply(){
     int A[3][2] = {{1, 4}, {2, 5}, {3, 6}};
     int B[2][3] = {{7, 8, 9}, {10, 11, 12}};
