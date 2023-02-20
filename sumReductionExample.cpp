08900d19e8098de2517e10ef101b554320954eb0
Sofía Palacios <77463414+JBSspc@users.noreply.github.com>
2023-02-20 13:13:35 -0600

    Update sumReductionExample.cpp

--- sumReductionExample.cpp
+++ sumReductionExample.cpp
@@ -5,72 +5,92 @@
 #include <chrono>
 #include <cmath>
 
+
 using namespace std;
 using namespace chrono;
 
+#define N 90000000
+
 // Definición de funciones
-void parallelMultiply();
-void serialMultiply();
-void initMat();
+float serialSum(vector <int> A, size_t n);
+float parallelSum(vector <int> A, size_t n);
+void initVectors();
+void verify();
 
 // Definición de variables globales
-//int A[2][2];
-//int B[2][2];
-int serialM[4][4];
-int parallelM[4][4];
+vector<int> A(N);
+float totalS;
+float totalP;
+
+
 
 int main() {
-
-    // Multiplicación serial
+    // Inicializar vectores con números aleatorios
     auto start = high_resolution_clock::now();
-    serialMultiply();
+    initVectors();
     auto end = high_resolution_clock::now();
-    auto serialTimer = duration_cast<microseconds>(end - start);
-    cout << "Serial matrix mult finished, elapsed time: "<<  serialTimer.count()/1000 << " milliseconds" <<endl;
+    auto duration = duration_cast<microseconds>(end - start);
+    cout << "Initialization finished, elapsed time: "<<  duration.count()/1000 << " milliseconds" <<endl;
 
-    // Multiplicación paralela
+    // Realizar suma serial
     start = high_resolution_clock::now();
-    parallelMultiply();
+    serialSum(A,N);
+    end = high_resolution_clock::now();
+    auto serialTimer = duration_cast<microseconds>(end - start);
+    cout << "Serial sum finished, elapsed time: "<<  serialTimer.count()/1000 << " milliseconds" <<endl;
+
+
+    // Realizar suma paralela
+    start = high_resolution_clock::now();
+    parallelSum(A,N);
     end = high_resolution_clock::now();
     auto parallelTimer = duration_cast<microseconds>(end - start);
-    cout << "Parallel matrix mul finished, elapsed time: "<<  parallelTimer.count()/1000 << " milliseconds" <<endl;
+    cout << "Parallel sum finished, elapsed time: "<<  parallelTimer.count()/1000 << " milliseconds" <<endl;
+
+
+    // Verificar que los resultados son iguales
+    verify();
 
     // Calcular speedUp
     cout << "SpeedUp = " << float(serialTimer.count())/float(parallelTimer.count()) << endl;
 
-    //MultiplyWithOutAMP();
-    //getchar();
+
+    return 0;
 }
 
-void serialMultiply(){
-    int A[4][3] = {{1, 4, 2}, {2, 5, 3}, {3, 6,7}, {4,8,9}};
-    int B[3][4] = {{7, 8, 9,15}, {10, 11, 12,9}, {10,5,6,2}};
-    for (int row = 0; row < 4; row++) {
-        for (int col = 0; col < 4; col++) {
-            // Multiply the row of A by the column of B to get the row, column of product.
-            for (int inner = 0; inner < 3; inner++) {
-                serialM[row][col] += A[row][inner] * B[inner][col];
-            }
-            cout << serialM[row][col] << "  ";
-        }
-        cout << "\n";
-    }
-    }
 
-void parallelMultiply(){
-    int A[4][3] = {{1, 4, 2}, {2, 5, 3}, {3, 6,7}, {4,8,9}};
-    int B[3][4] = {{7, 8, 9,15}, {10, 11, 12,9}, {10,5,6,2}};
-//#pragma omp parallel for
-    for (int row = 0; row < 4; row++) { // Efecto sobre las filas
-        //#pragma omp parallel for
-        for (int col = 0; col < 4; col++) { // Efecto sobre las columnas
-        #pragma omp parallel for
-            // Multiply the row of A by the column of B to get the row, column of product.
-            for (int inner = 0; inner < 3; inner++) { // Efecto sobre filas y columnas
-                parallelM[row][col] += A[row][inner] * B[inner][col];
-            }
-            cout << parallelM[row][col] << "  ";
-        }
-        cout << "\n";
+/*
+* FunCtion to initialize two vectors A and B randomly.
+*/
+void initVectors(){
+    //#pragma omp parallel for
+    for(int i=0; i<N; i++){
+        A[i] =  i * 2; // Rnd Nums 1 - 100
     }
+}
+
+float serialSum(vector <int> A, size_t n){
+    float totalS=0.;
+    for(size_t i= 0; i<N; i++){
+        totalS += A[i];
+    }
+    return totalS;
+}
+
+float parallelSum(vector <int> A, size_t n){
+    float totalP=0.;
+#pragma omp parallel for reduction(+:totalP)
+    for(size_t i= 0; i<N; i++){
+        totalP += A[i];
+    }
+    return totalP;
+
+}
+
+void verify(){
+    if(totalS != totalP)
+        cout<< 0 << endl;
+    else
+        cout <<1<<endl;
+
 }
